2f7b8f04334f8c68c1d6bd73823ab8930797d1c7
Cicero Ewerton <cicero.ewerton@gmail.com>
2024-03-30 13:10:31 -0300

    JavaScript

--- Explorer/JavaScript/script.js
+++ Explorer/JavaScript/script.js
@@ -1,3 +1,52 @@
-console.log('Bem vindos ao Starter!');
+// let nome = prompt("Qual seu nome?");
+// alert("Olá, " + nome);
 
-console.log('ano ' + 20);
+// alert("Iremos somar dois numeros!")
+// let numberOne = prompt("Digite o primeiro numero")
+// let numberTwo = prompt("Digite o segundo numero")
+// let result = Number(numberOne) + Number(numberTwo)
+
+// alert("Resultado " + result)
+
+// let aluno = prompt("Qual seu nome?");
+// let nota1 = prompt("Qual sua nota da primeira prova?");
+// let nota2 = prompt("Qual sua nota da segunda prova?");
+// let nota3 = prompt("Qual sua nota da terceira prova?");
+
+// let media = (Number(nota1) + Number(nota2) + Number(nota1)) / 3;
+// media = media.toFixed(2)
+
+// if (media >= 6) {
+//   alert("Parabéns, " + aluno + "! Sua média foi de: " + media);
+// } else {
+//   alert(aluno + " estude para sua prova de recuperação! Sua média foi de: " +  media);
+// }
+
+// let itens = [];
+
+// for (item = 0; item < 2; item++){
+//    let itemNome = prompt('Qual o item' + (item + 1))
+//     itens [item] = itemNome
+// }
+
+// alert(itens)
+
+
+
+
+// let resultado = prompt("Adivinhe o numero que estou pensando, de 1 a 10!");
+
+// const gerar = Math.round(Math.random() * 10);
+
+// let tentativas = 1;
+
+// while (Number(resultado) != gerar) {
+//   resultado = prompt("Tente novamente!");
+
+//   tentativas++;
+// }
+
+// alert(`Parabéns você acertou em ${tentativas} tentativa(s)!`);
+
+
+
